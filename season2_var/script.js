212127a415f7ad899ba4b35a83c746d8c54ada09
Manfred <Escape>
2020-02-15 23:05:50 +0700

    bài 2 . biến

---
+++ season2_var/script.js
@@ -0,0 +1,21 @@
+alert("Xin chao CE4JS 71"); //thong bao         
+
+prompt("Ten ban la gi");  //nhap thong tin tu nguoi dung
+
+let age;    //khai bao bien
+age = 25;    //gan gia tri
+
+let age1=25;
+var name;
+const PI=3.14;
+
+//su dung
+// alert(age);
+// alert(age1);
+console.log(age);
+console.log(age1);
+let a =5;
+let b=10;
+let c= a+b ;
+console.log(c);
+
